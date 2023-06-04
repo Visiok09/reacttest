bd045fc66f1532bb48a3b78a594f545b639f41b5
Visiok09 <sm3729540@gmail.com>
2023-06-04 21:48:28 +0300

    modified

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import Post from "./Post";
 
-export default function Posts (props){
-    return <div>
-        {
-            props.posts.map(post => (
-                <Post key={post.id} name={post.name}/>
-            ))
-        }
-    </div>
+export default function Posts(props){
+    const { posts, removePost } = props;
+  
+    return (
+        <div>
+            {posts.map(post => (
+                <Post key={post.id} id={post.id} name={post.name} removePost={removePost} />
+            ))}
+        </div>
+    );
 }
